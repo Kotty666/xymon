a946537f7d0d15a5e50c12b3eab2a7d693b72218
Christoph Berg <myon@debian.org>
2007-10-12 23:20:45 +0200

    * Drop "hobbit" binary again, instead bbcmd will set PATH to include   the hobbit binaries. * Add /etc/hobbit/hobbitlaunch.d/ directory for server extensions. * When upgrading from a pre-Debian version, move old directories in   /var/lib/hobbit/www/ away, we want them to be symlinks. * Add COLUMS=200 to the dpkg -l call for Sarge compatibility. * Remove /usr/lib/hobbit/*/tmp/ symlinks, set BBTMP="/var/lib/hobbit/tmp". * Add some examples to the default bb-hosts file. * Move the config file tweaking from debian/rules to quilt.

---
+++ init-common.sh
@@ -0,0 +1,27 @@
+# common init functions for hobbit and hobbit-client
+
+create_includefiles ()
+{
+	if [ "$HOBBITSERVERS" = "" ]; then
+		echo "Please configure HOBBITSERVERS in /etc/default/hobbit-client"
+		exit 0
+	fi
+
+	set -- $HOBBITSERVERS
+	if [ $# -eq 1 ]; then
+		echo "BBDISP=\"$HOBBITSERVERS\""
+		echo "BBDISPLAYS=\"\""
+	else
+		echo "BBDISP=\"0.0.0.0\""
+		echo "BBDISPLAYS=\"$HOBBITSERVERS\""
+	fi > /var/run/hobbit/bbdisp-runtime.cfg
+
+	for cfg in /etc/hobbit/clientlaunch.d/*.cfg ; do
+		test -e $cfg && echo "include $cfg"
+	done > /var/run/hobbit/clientlaunch-include.cfg
+
+	test -d /etc/hobbit/hobbitlaunch.d || return
+	for cfg in /etc/hobbit/hobbitlaunch.d/*.cfg ; do
+		test -e $cfg && echo "include $cfg"
+	done > /var/run/hobbit/hobbitlaunch-include.cfg
+}
