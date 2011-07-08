2b8e92ce6a1e92f4b7f6116e9c6ec04b644d9e28
Christoph Berg <myon@debian.org>
2011-07-08 10:54:50 +0200

    Imported Debian patch 4.3.0~beta2.dfsg-9

---
+++ debian/init-common.sh
@@ -0,0 +1,55 @@
+# common init functions for hobbit and hobbit-client
+
+create_includefiles ()
+{
+	if [ "$HOBBITSERVERS" = "" ]; then
+		echo "Please configure HOBBITSERVERS in /etc/default/hobbit-client"
+		exit 0
+	fi
+
+	umask 022
+
+	if ! [ -d /var/run/hobbit ] ; then
+		mkdir /var/run/hobbit
+		chown hobbit:hobbit /var/run/hobbit
+	fi
+
+	set -- $HOBBITSERVERS
+	if [ $# -eq 1 ]; then
+		echo "BBDISP=\"$HOBBITSERVERS\""
+		echo "BBDISPLAYS=\"\""
+	else
+		echo "BBDISP=\"0.0.0.0\""
+		echo "BBDISPLAYS=\"$HOBBITSERVERS\""
+	fi > /var/run/hobbit/bbdisp-runtime.cfg
+
+	for cfg in /etc/hobbit/clientlaunch.d/*.cfg ; do
+		test -e $cfg && echo "include $cfg"
+	done > /var/run/hobbit/clientlaunch-include.cfg
+
+	if test -d /etc/hobbit/hobbitlaunch.d ; then
+		for cfg in /etc/hobbit/hobbitlaunch.d/*.cfg ; do
+			test -e $cfg && echo "include $cfg"
+		done > /var/run/hobbit/hobbitlaunch-include.cfg
+	fi
+
+	if test -d /etc/hobbit/hobbitgraph.d ; then
+		for cfg in /etc/hobbit/hobbitgraph.d/*.cfg ; do
+			test -e $cfg && echo "include $cfg"
+		done > /var/run/hobbit/hobbitgraph-include.cfg
+	fi
+
+	if test -d /etc/hobbit/hobbitserver.d ; then
+		for cfg in /etc/hobbit/hobbitserver.d/*.cfg ; do
+			test -e $cfg && echo "include $cfg"
+		done > /var/run/hobbit/hobbitserver-include.cfg
+	fi
+
+	if test -d /etc/hobbit/hobbitclient.d ; then
+		for cfg in /etc/hobbit/hobbitclient.d/*.cfg ; do
+			test -e $cfg && echo "include $cfg"
+		done > /var/run/hobbit/hobbitclient-include.cfg
+	fi
+
+	return 0
+}
