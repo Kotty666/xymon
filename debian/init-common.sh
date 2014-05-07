ff3cc5b4c71c2bc8624a65d53114bd24dfd8a9b7
Christoph Berg <myon@debian.org>
2014-05-07 14:14:31 +0200

    Always write /var/run/xymon/xymonclient-include.cfg on clients

--- debian/init-common.sh
+++ debian/init-common.sh
@@ -27,6 +27,10 @@ create_includefiles ()
 		test -e $cfg && echo "include $cfg"
 	done > /var/run/xymon/clientlaunch-include.cfg
 
+	for cfg in /etc/xymon/xymonclient.d/*.cfg ; do
+		test -e $cfg && echo "include $cfg"
+	done > /var/run/xymon/xymonclient-include.cfg
+
 	if test -x /usr/lib/xymon/server/bin/xymond ; then
 		for cfg in /etc/xymon/xymonlaunch.d/*.cfg ; do
 			test -e $cfg && echo "include $cfg"
@@ -39,10 +43,6 @@ create_includefiles ()
 		for cfg in /etc/xymon/xymonserver.d/*.cfg ; do
 			test -e $cfg && echo "include $cfg"
 		done > /var/run/xymon/xymonserver-include.cfg
-
-		for cfg in /etc/xymon/xymonclient.d/*.cfg ; do
-			test -e $cfg && echo "include $cfg"
-		done > /var/run/xymon/xymonclient-include.cfg
 	fi
 
 	return 0
